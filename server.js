1218338d53a3bf62f86f6503c59eb0721ac63585
ykzZa <n.hryshaienko@gmail.com>
2022-12-13 21:49:19 +0200

    new

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const PORT = 80;
 const HOST = '0.0.0.0';
 
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello DevOps from Nikita Hryshaienko');
+  res.status(200).send('Hello DevOps from Nikita Hryshaienko');
 });
 
 app.listen(PORT, HOST, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
+
+module.exports = app;
